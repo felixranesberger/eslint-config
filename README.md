4af23936b88ebe979898bb569ad0505f01ea23d4
Felix Ranesberger <felix.ranesberger@in2code.de>
2023-03-30 15:40:22 +0200

    [TASK] update package.json configuration and change namespace

---
+++ README.md
@@ -0,0 +1,2 @@
+# eslint-config-franesberger
+ESLint configuration based on the awesome [@antfu/eslint-config](https://github.com/antfu/eslint-config) preset.
---
+++ index.js
@@ -0,0 +1,20 @@
+process.env.ESLINT_TSCONFIG = 'tsconfig.json';
+
+module.exports = {
+    extends: '@antfu',
+    rules: {
+        'no-console': 'warn',
+        'semi': ['error', 'always'],
+        'semi-style': ['error', 'last'],
+        '@typescript-eslint/semi': ['error', 'always'],
+        'curly': ['error', 'multi-line'],
+        'object-curly-spacing': ['error', 'always'],
+        'object-curly-newline': ['error', {
+            ObjectExpression: { minProperties: 4, multiline: true, consistent: true },
+            ObjectPattern: { minProperties: 4, multiline: true, consistent: true },
+            ImportDeclaration: { minProperties: 4, multiline: true, consistent: true },
+            ExportDeclaration: { minProperties: 4, multiline: true, consistent: true },
+        }],
+        'antfu/if-newline': 'off',
+    },
+};
